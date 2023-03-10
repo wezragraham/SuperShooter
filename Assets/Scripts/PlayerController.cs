25844695fe483ae228c77f87858ef9a17aa4f360
wezragraham <wezragraham@gmail.com>
2023-03-10 16:41:47 -0700

    player can now move around room
    player switches to opposite side of room if they reach a wall

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -30,9 +30,32 @@ public class PlayerController : MonoBehaviour
         _vInput = Input.GetAxis("Vertical");
         _mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
 
-        transform.Translate(Vector3.forward * _vInput * _movementSpeedModifier * Time.deltaTime);
+        //if position is within this area, move
+        if ((transform.position.x >= -11 && transform.position.x <= 11) || (transform.position.z >= -11 && transform.position.z <= 11))
+        {
+            transform.Translate(Vector3.forward * _vInput * _movementSpeedModifier * Time.deltaTime);
+            transform.Translate(Vector3.right * _hInput * _movementSpeedModifier * Time.deltaTime);
+        }
 
-        transform.Translate(Vector3.right * _hInput * _movementSpeedModifier * Time.deltaTime);
+        //if x position is less than neg eleven or greater than eleven, move to other side of wall
+        if (transform.position.x <= -11)
+        {
+            transform.position = new Vector3(10, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x >= 11)
+        {
+            transform.position = new Vector3(-11, transform.position.y, transform.position.z);
+        }
+
+        //if z position is less than neg eleven or greater than eleven, move to other side of wall
+        if (transform.position.z <= -11)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, 10);
+        }
+        if (transform.position.z >= 11)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -10);
+        }
 
         transform.Rotate(Vector3.up * _mouseX * _rotationSpeedModifier * Time.deltaTime);
 
